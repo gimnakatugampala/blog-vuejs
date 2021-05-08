8bbcb4028a2f3027da77c3636849899fddde99a6
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-05-08 18:11:51 +0530

    added the tags ompoenents

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,15 +2,19 @@
    <div class="card-body">
     <h5 class="card-title">{{post.text}}</h5>
     <p class="card-text">{{post.body}}.</p>
-    <a href="#" class="btn btn-primary">Button</a>
+    <Tags :tags="post.tags"/>
   </div>
 </template>
 
 <script>
+import Tags from './Tags'
 export default {
     name:'Post',
     props:{
         post:Object
+    },
+    components:{
+        Tags
     }
 
 }
---
+++ src/components/Tags.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href="#" class="btn btn-primary m-1" v-for="tag in tags" :key="tag">{{tag}}</a>
+</template>
+
+<script>
+export default {
+    name:'Tags',
+    props:{
+        tags:Array
+    }
+
+}
+</script>
+
+<style>
+
+</style>
